d07e3802d017ff583bc35dab3f8ca918482ca1a0
biancavonposer <68602470+biancavonposer@users.noreply.github.com>
2021-07-17 14:49:24 -0300

    Upgrading program
    I added some features to my cookbook using hashes so the client could register, along with the name, the type of the recipe.

--- cookbook.rb
+++ cookbook.rb
@@ -1,7 +1,8 @@
 puts 'Bem-vindo ao Cookbook, sua rede social de receitas.'
-
+puts
 receitas = []
-puts '[1] Cadastrar o nome de uma receita'
+tipos = []
+puts '[1] Cadastrar uma receita'
 puts '[2] Exibir menu de receitas'
 puts '[3] Sair'
 
@@ -13,13 +14,22 @@ while (opcao != 3) do
   if opcao == 1
     puts 'Digite o nome da receita: '
     nome_receita = gets.chomp
-    receitas << nome_receita
+    puts 'Digite o tipo de receita: '
+    tipo_receita = gets.chomp
+    
+    receitas << { nome: nome_receita, tipo: tipo_receita }
+    puts
     puts "Receita #{nome_receita} cadastrada com sucesso!"
+    puts
   elsif opcao == 2
     puts '======== Receitas Cadastradas ========'
-    puts receitas
+    receitas.each do
+      puts "#{nome_receita} - #{tipo_receita}"
+    end
+    puts    
   else
     puts 'Opção inválida.'
+    puts
   end
 
 puts '[1] Cadastrar o nome de uma receita'
