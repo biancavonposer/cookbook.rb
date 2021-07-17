e2acfd66cbb594065029f18d68c04840c715f23d
biancavonposer <68602470+biancavonposer@users.noreply.github.com>
2021-07-17 15:53:59 -0300

    Upgrade 2
    Here, I corrected indentation and made my code cleaner and organized in the name of good habits.

--- cookbook.rb
+++ cookbook.rb
@@ -1,43 +1,58 @@
-puts 'Bem-vindo ao Cookbook, sua rede social de receitas.'
-puts
-receitas = []
-tipos = []
-puts '[1] Cadastrar uma receita'
-puts '[2] Exibir menu de receitas'
-puts '[3] Sair'
+def bem_vindo
+  puts 'Bem-vindo ao Cookbook, sua rede social de receitas.'
+end
 
-print 'Escolha uma opção: '
-opcao = gets.to_i
+def menu
+  puts '[1] Cadastrar uma receita'
+  puts '[2] Exibir menu de receitas'
+  puts '[3] Sair'
+
+  print 'Escolha uma opção: '
+  return gets.to_i
+end
+
+def inserir_receita
+  puts 'Digite o nome da receita: '
+  nome_receita = gets.chomp
+  puts 'Digite o tipo de receita: '
+  tipo_receita = gets.chomp
+
+  puts
+  puts "Receita #{nome_receita} cadastrada com sucesso!"
+  puts
+  return { nome: nome_receita,tipo: tipo_receita }
+end
+
+def imprimir_receitas(receitas)
+  puts '======== Receitas Cadastradas ========'
+    receitas.each do |receita|
+      puts "#{receita[:nome]} - #{receita[:tipo]}"
+    end
+  puts
+end
+
+
+ 
+
+bem_vindo
+
+receitas = []
+
+opcao = menu
+
 
 
 while (opcao != 3) do 
   if opcao == 1
-    puts 'Digite o nome da receita: '
-    nome_receita = gets.chomp
-    puts 'Digite o tipo de receita: '
-    tipo_receita = gets.chomp
-    
-    receitas << { nome: nome_receita, tipo: tipo_receita }
-    puts
-    puts "Receita #{nome_receita} cadastrada com sucesso!"
-    puts
+    receitas << inserir_receita
   elsif opcao == 2
-    puts '======== Receitas Cadastradas ========'
-    receitas.each do
-      puts "#{nome_receita} - #{tipo_receita}"
-    end
-    puts    
+    imprimir_receitas(receitas)
   else
     puts 'Opção inválida.'
     puts
   end
 
-puts '[1] Cadastrar o nome de uma receita'
-puts '[2] Exibir menu de receitas'
-puts '[3] Sair'
-
-print 'Escolha uma opção: '
-opcao = gets.to_i
+opcao = menu
 
 end
 
